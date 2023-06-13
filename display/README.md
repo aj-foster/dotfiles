b08927ec047250ea28d5fcce12598b5bdde5cddb
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2023-06-12 22:18:42 -0400

    Add display scripts for Stream Deck

---
+++ display/README.md
@@ -0,0 +1,19 @@
+# Display Support
+
+_For Stream Deck_
+
+---
+
+First, install [displayplacer](https://github.com/jakehilborn/displayplacer):
+
+```shell
+brew tap jakehilborn/jakehilborn && brew install displayplacer
+```
+
+Then, prepare the scripts:
+
+```shell
+chmod +x ~/.files/display/*.sh
+```
+
+Finally, create an "Open" key for each script on the Stream Deck.
---
+++ display/wide.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+/opt/homebrew/bin/displayplacer \
+  "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:2056x1329 hz:120 color_depth:8 enabled:true scaling:on origin:(0,0) degree:0" \
+  "id:CC30B1FE-777F-46A8-84B0-477120576209 res:5120x1440 hz:60 color_depth:7 enabled:true scaling:off origin:(-5120,-106) degree:0" \
+  "id:9CC87DA7-8CE0-42D2-8FF4-AC1A82A30DB2 enabled:false"
